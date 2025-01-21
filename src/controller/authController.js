9d81b79ac6c5718f837e0e63167c538b555026b8
Trần Xuân Đông <shin@Trans-MacBook-Pro.local>
2025-01-21 10:22:49 +0700

    update

--- src/controller/authController.js
+++ src/controller/authController.js
@@ -3,10 +3,10 @@ const AuthService = require('../services/authService');
 
 class AuthController {
     // Middleware cho login với Google
-    login = (req, res, next) => {
-        console.log("Login request received");
-        passport.authenticate('google', { scope: ['profile', 'email'] })(req, res, next);
-      };
+    // login = (req, res, next) => {
+    //     console.log("Login request received");
+    //     passport.authenticate('google', { scope: ['profile', 'email'] })(req, res, next);
+    //   };
 
       googleCallback(req, res) {
         const { token } = req.user;
