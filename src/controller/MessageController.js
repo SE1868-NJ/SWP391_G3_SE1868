f045c87fb60d074a5e089c93d1dca0fdd95921a8
Tran Xuan Dong <dongtx04@gmail.com>
2025-02-12 23:42:04 +0700

    update

--- src/controller/MessageController.js
+++ src/controller/MessageController.js
@@ -11,7 +11,6 @@ class MessageController extends BaseController {
 
             return this.convertToJson(res, 200, result);
         } catch (error) {
-            // console.error('Lỗi khi lấy tin nhắn:', error);
             return this.handleError(res, error);
         }
     }
