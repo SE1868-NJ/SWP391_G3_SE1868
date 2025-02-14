f2d2a9f9de36a9504c7e31c61c06cc4f9bc1f869
Tran Xuan Dong <dongtx04@gmail.com>
2025-02-14 14:35:16 +0700

    update

--- src/controller/ConversationController.js
+++ src/controller/ConversationController.js
@@ -5,14 +5,13 @@ class ConversationController extends BaseController {
     getShopConversations = async (req, res) => {
         try {
             const { shopId } = req.params;
-            console.log(shopId);
             const conversations = await conversationService.getShopConversations(shopId);
             return this.convertToJson(res, 200, conversations);
         } catch (error) {
             return this.handleError(res, error);
         }
     }
-    
+
     getUserConversations = async (req, res) => {
         try {
             const { userId } = req.params;
--- src/services/conversationService.js
+++ src/services/conversationService.js
@@ -1,15 +1,31 @@
+const { json } = require('sequelize');
 const conversationRepository = require('../repositories/conversationRepository');
+const shopRepository = require('../repositories/shopRepository');
 
 class ConversationService {
     constructor() {
     }
 
     async getUserConversations(userId) {
-        return await conversationRepository.getConversationsByUser(userId);
+        let data = [];
+        try {
+            data = await conversationRepository.getConversationsByUser(userId);
+            for (let conversation of data) {
+                const shop = await shopRepository.getShopById(conversation.shop_id);
+                conversation.dataValues.shop = shop.dataValues;
+            }
+        } catch (error) {
+            throw new Error(`Error: ${error.message}`);
+        }
+        return data;
     }
 
     async getShopConversations(shopId) {
-        return await conversationRepository.getConversationsByShop(shopId);
+        try {
+            return await conversationRepository.getConversationsByShop(shopId);
+        } catch (error) {
+            throw new Error(`Error: ${error.message}`);
+        }
     }
 }
 
