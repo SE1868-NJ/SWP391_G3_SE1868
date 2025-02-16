efb7ea06d8777ac61abb058422c143af358e623c
ptt98hd <thanhpham.hd.98@gmail.com>
2025-02-17 02:21:02 +0700

    update user table columns

--- src/migrations/20250216152456-create-users.js
+++ src/migrations/20250216152456-create-users.js
@@ -2,21 +2,65 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
-  },
+	async up(queryInterface, Sequelize) {
+		await queryInterface.createTable('users', {
+			user_id: {
+				type: Sequelize.INTEGER,
+				allowNull: false,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			google_id: {
+				type: Sequelize.STRING,
+				unique: true,
+			},
+			facebook_id: {
+				type: Sequelize.STRING,
+				unique: true,
+			},
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+			avatar: {
+				type: Sequelize.STRING,
+			},
+			full_name: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			gender: {
+				type: Sequelize.ENUM('male', 'female', 'other'),
+				allowNull: false,
+			},
+			date_of_birth: {
+				type: Sequelize.DATEONLY,
+				allowNull: false,
+			},
+
+			phone: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			email: {
+				type: Sequelize.STRING,
+				allowNull: false,
+			},
+			password: {
+				type: Sequelize.STRING,
+				allowNull: true,
+			},
+			created_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+				defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+			},
+			updated_at: {
+				type: Sequelize.DATE,
+				allowNull: false,
+				defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+			},
+		});
+	},
+
+	async down(queryInterface, Sequelize) {
+		await queryInterface.dropTable('users');
+	},
 };
