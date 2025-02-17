9fb3c43567fe3427fc380a84414bff92b21cab51
ptt98hd <thanhpham.hd.98@gmail.com>
2025-02-18 00:28:05 +0700

    change foreign key from shop table to user table to user_id

--- src/migrations/20250205070838-create-shop.js
+++ src/migrations/20250205070838-create-shop.js
@@ -25,7 +25,7 @@ module.exports = {
 				allowNull: false,
 				references: {
 					model: 'Users',
-					key: 'id',
+					key: 'user_id',
 				},
 				onUpdate: 'CASCADE',
 				onDelete: 'CASCADE',
--- src/models/shop.js
+++ src/models/shop.js
@@ -2,51 +2,57 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-    class Shop extends Model {
-        static associate(models) {
-            // Shop.hasMany(models.Product, { foreignKey: 'shop_id', onDelete: 'CASCADE' });
-            Shop.belongsTo(models.User, { foreignKey: 'user_id', onDelete: 'CASCADE' });
-        }
-    }
-    Shop.init({
-        shop_id: {
-            type: DataTypes.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        user_id: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-            references: {
-                model: 'User',
-                key: 'user_id'
-            }
-        },
-        shop_name: {
-            type: DataTypes.STRING,
-            allowNull: false
-        },
-        shop_description: {
-            type: DataTypes.STRING
-        },
-        shop_logo: {
-            type: DataTypes.STRING
-        },
-        shop_address: {
-            type: DataTypes.STRING
-        },
-        created_at: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW
-        },
-        updated_at: {
-            type: DataTypes.DATE,
-            defaultValue: DataTypes.NOW
-        }
-    }, {
-        sequelize,
-        modelName: 'Shop',
-        timestamps: false
-    });
-    return Shop;
+	class Shop extends Model {
+		static associate(models) {
+			// Shop.hasMany(models.Product, { foreignKey: 'shop_id', onDelete: 'CASCADE' });
+			Shop.belongsTo(models.User, {
+				foreignKey: 'user_id',
+				onDelete: 'CASCADE',
+			});
+		}
+	}
+	Shop.init(
+		{
+			shop_id: {
+				type: DataTypes.INTEGER,
+				primaryKey: true,
+				autoIncrement: true,
+			},
+			user_id: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+				references: {
+					model: 'User',
+					key: 'user_id',
+				},
+			},
+			shop_name: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			shop_description: {
+				type: DataTypes.STRING,
+			},
+			shop_logo: {
+				type: DataTypes.STRING,
+			},
+			shop_address: {
+				type: DataTypes.STRING,
+			},
+			created_at: {
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
+			updated_at: {
+				type: DataTypes.DATE,
+				defaultValue: DataTypes.NOW,
+			},
+		},
+		{
+			sequelize,
+			modelName: 'Shop',
+			timestamps: false,
+		}
+	);
+	return Shop;
 };
