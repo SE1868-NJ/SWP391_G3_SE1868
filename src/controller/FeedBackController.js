d7b5778835562e5a0d62e1e25d321861f6040671
Tran Xuan Dong <dongtx04@gmail.com>
2025-02-18 22:51:21 +0700

    update 18_2

--- src/controller/FeedBackController.js
+++ src/controller/FeedBackController.js
@@ -3,11 +3,22 @@ const BaseController = require('./baseController');
 
 class FeedBackController extends BaseController {
 
-    getFeedBacks = async (req, res) => {
-        try {
-            const params = { page: parseInt(req.body.page), product_id: parseInt(req.body.product_id) };
+    // getFeedBacks = async (req, res) => {
+    //     try {
+    //         const params = { page: parseInt(req.body.page), product_id: parseInt(req.body.product_id) };
 
-            const result = await feedbackService.getFeedBacks(params);
+    //         const result = await feedbackService.getFeedBacks(params);
+    //         this.convertToJson(res, 200, result);
+    //     } catch (error) {
+    //         this.handleError(res, error);
+    //     }
+    // }
+
+    getFeedBacksByProductId = async (req, res) => {
+        try {
+            const product_id = parseInt(req.params.id);
+
+            const result = await feedbackService.getFeedBacks(product_id);
             this.convertToJson(res, 200, result);
         } catch (error) {
             this.handleError(res, error);
