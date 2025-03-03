9a10707618d04bbdf344fd12fd27551d26a23bbc
Meou-83 <anpthe186638@fpt.edu.vn>
2025-03-03 09:30:57 +0700

    fix_conflict

--- src/routers/shoprouter.js
+++ src/routers/shoprouter.js
@@ -8,12 +8,14 @@ router.get('/category/get_list_category', shopController.getCategory);
 
 //product
 router.post('/product/get_list_product', shopController.getProducts);
-router.get('/product/get_feedbacks_by_product', feedBackController.getFeedBacks);
-router.get('/product/get_product_by_id', shopController.getProductById);
-
+router.get('/product/get_feedbacks_by_product/:id', feedBackController.getFeedBacksByProductId);
+router.get('/product/get_product_by_id/:id', shopController.getProductById);
 // cart
 router.get('/cart/get_cart_by_user/:id', shopController.getCartsByUserId);
 router.post('/cart/add_to_cart', shopController.createCart);
 router.get('/cart/get_count_cart_by_user/:id', shopController.getCountCartByUserId);
+
+// Shop || seller
+router.get('/get_shop_by_user/:id', shopController.getShopByUserId);
 
 module.exports = router;
