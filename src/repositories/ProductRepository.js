ab3f26f90144387b3b95ede76484ea2f6ff46a2e
Tran Xuan Dong <dongtx04@gmail.com>
2025-03-07 08:40:53 +0700

    update

--- src/repositories/ProductRepository.js
+++ src/repositories/ProductRepository.js
@@ -1,117 +1,125 @@
 const db = require('../models');
 
 class ProductRepository {
-    constructor() {
+  constructor() {
+  }
+
+  async getAllProduct() {
+    return await db.Product.findAll();
+  }
+
+  async getProducts(params) {
+    const { page = 1, limit = 4, search, sortPrice, categories = [], minPrice, maxPrice } = params;
+
+    const whereClause = {
+      status: 'active'
+    };
+
+    if (search && search.trim() !== "") {
+      whereClause.product_name = { [db.Sequelize.Op.like]: `%${search}%` };
     }
 
-    async getAllProduct() {
-        return await db.Product.findAll();
+    // Lọc theo khoảng giá
+    if (minPrice && maxPrice) {
+      whereClause.sale_price = { [db.Sequelize.Op.between]: [minPrice, maxPrice] };
+    } else if (minPrice) {
+      whereClause.sale_price = { [db.Sequelize.Op.gte]: minPrice };
+    } else if (maxPrice) {
+      whereClause.sale_price = { [db.Sequelize.Op.lte]: maxPrice };
     }
 
-    async getProducts(params) {
-        const { page = 1, limit = 4, search, sortPrice, categories=[], minPrice, maxPrice } = params;
-    
-        const whereClause = {
-          status: 'active'
-        };
-    
-        if (search && search.trim() !== "") {
-          whereClause.product_name = { [db.Sequelize.Op.like]: `%${search}%` };
-        }
-        
-        // Lọc theo khoảng giá
-        if (minPrice && maxPrice) {
-          whereClause.sale_price = { [db.Sequelize.Op.between]: [minPrice, maxPrice] };
-      } else if (minPrice) {
-          whereClause.sale_price = { [db.Sequelize.Op.gte]: minPrice };
-      } else if (maxPrice) {
-          whereClause.sale_price = { [db.Sequelize.Op.lte]: maxPrice };
-      }
-    
-      // Lọc theo danh mục
-      if (categories.length > 0) {
-          whereClause['$category.category_name$'] = { [db.Sequelize.Op.in]: categories };
-      }
-    
-        const order = [];
-        if (sortPrice) {
-          order.push(["sale_price", sortPrice === "desc" ? "DESC" : "ASC"]);
-        } else {
-          order.push(["createdAt", "DESC"]);
-        }
-    
-        const { count, rows } = await db.Product.findAndCountAll({
-          where: whereClause,
-          include: [{
-              model: db.Category,
-              as: 'category'
-            }],
-          limit: parseInt(limit),
-          offset: (page - 1) * limit,
-          order: order,
-        });
-    
-        return {
-          items: rows,
-          metadata: {
-            total: count,
-            page: parseInt(page),
-            limit: parseInt(limit),
-            totalPages: Math.ceil(count / limit)
-          }
-        };
-      }
-    
-
-    async getProductById(id) {
-        return await db.Product.findByPk(id, {
-            include: [{
-                model: db.Shop,
-                as: 'shop'
-            }]
-        });
+    // Lọc theo danh mục
+    if (categories.length > 0) {
+      whereClause['$category.category_name$'] = { [db.Sequelize.Op.in]: categories };
     }
-    async updateSearchCount(productId) {
-        try {
-          // Sửa lỗi: Thay thế Product bằng db.Product
-          const product = await db.Product.findByPk(productId);
-          if (!product) {
-            throw new Error("Product not found");
-          }
-    
-          // Cập nhật search_count một cách rõ ràng với giá trị cụ thể
-          product.search_count = product.search_count + 1;
-          await product.save();
-    
-          return product;
-        } catch (error) {
-          throw new Error(`Error updating search count: ${error.message}`);
-        }
+
+    const order = [];
+    if (sortPrice) {
+      order.push(["sale_price", sortPrice === "desc" ? "DESC" : "ASC"]);
+    } else {
+      order.push(["createdAt", "DESC"]);
+    }
+
+    const { count, rows } = await db.Product.findAndCountAll({
+      where: whereClause,
+      include: [{
+        model: db.Category,
+        as: 'category'
+      }],
+      limit: parseInt(limit),
+      offset: (page - 1) * limit,
+      order: order,
+    });
+
+    return {
+      items: rows,
+      metadata: {
+        total: count,
+        page: parseInt(page),
+        limit: parseInt(limit),
+        totalPages: Math.ceil(count / limit)
       }
-    
-      async getMostSearchedProducts(limit = 4) {
-        try {
-          // Sửa lỗi: Thay thế Product bằng db.Product
-          return await db.Product.findAll({
-            where: {
-              status: "active", // Thêm điều kiện nếu cần
-            },
-            order: [["search_count", "DESC"]],
-            limit: parseInt(limit),
-            attributes: [
-              "id",
-              "product_name",
-              "search_count",
-              "image_url",
-              "sale_price",
-            ],
-          });
-        } catch (error) {
-          throw new Error(`Error getting most searched products: ${error.message}`);
-        }
-      }    
-    
-    
+    };
+  }
+
+
+  async getProductById(id) {
+    return await db.Product.findByPk(id, {
+      include: [{
+        model: db.Shop,
+        as: 'shop'
+      }]
+    });
+  }
+  async updateSearchCount(productId) {
+    try {
+      // Sửa lỗi: Thay thế Product bằng db.Product
+      const product = await db.Product.findByPk(productId);
+      if (!product) {
+        throw new Error("Product not found");
+      }
+
+      // Cập nhật search_count một cách rõ ràng với giá trị cụ thể
+      product.search_count = product.search_count + 1;
+      await product.save();
+
+      return product;
+    } catch (error) {
+      throw new Error(`Error updating search count: ${error.message}`);
+    }
+  }
+
+  async getMostSearchedProducts(limit = 4) {
+    try {
+      // Sửa lỗi: Thay thế Product bằng db.Product
+      return await db.Product.findAll({
+        where: {
+          status: "active", // Thêm điều kiện nếu cần
+        },
+        order: [["search_count", "DESC"]],
+        limit: parseInt(limit),
+        attributes: [
+          "id",
+          "product_name",
+          "search_count",
+          "image_url",
+          "sale_price",
+        ],
+      });
+    } catch (error) {
+      throw new Error(`Error getting most searched products: ${error.message}`);
+    }
+  }
+
+  async updateStockQuantity(product) {
+    return await db.Product.update(
+      { stock_quantity: product.stock_quantity },
+      {
+        where: { id: product.id },
+      }
+    );
+  }
+
 }
 
 module.exports = new ProductRepository();
--- src/services/orderService.js
+++ src/services/orderService.js
@@ -1,5 +1,6 @@
 const orderRepository = require('../repositories/OrderRepository');
 const cartRepository = require('../repositories/CartRepository');
+const productRepository = require('../repositories/ProductRepository');
 const orderDetailRepository = require('../repositories/OrderDetailRepository');
 
 class OrderService {
@@ -7,15 +8,24 @@ class OrderService {
     }
 
     async createOrder(data) {
-        const ItemProducts = data.items;
-        // update status cart order
-        for (let i = 0; i < ItemProducts.length; i++) {
-            const dataUpdate = await cartRepository.updateIsOrdered(data.user_id, ItemProducts[i].product_id);
-            console.log(dataUpdate);
+        try {
+            const ItemProducts = data.items;
+            //console.log(ItemProducts);
+            for (let i = 0; i < ItemProducts.length; i++) {
+                //update is_ordered in cart
+                const dataUpdate = await cartRepository.updateIsOrdered(data.user_id, ItemProducts[i].product_id);
+                //update stock_quantity in product
+                const product = await productRepository.getProductById(ItemProducts[i].product_id);
+                if (!product) {
+                    throw new Error("Product not found");
+                }
+                product.stock_quantity = product.stock_quantity - ItemProducts[i].quantity;
+                await productRepository.updateStockQuantity(product);
+            }
+            return await orderRepository.createOrder(data);
+        } catch (error) {
+            throw new Error(`Error creating order: ${error.message}`);
         }
-
-        // create order
-        return await orderRepository.createOrder(data);
     }
 
     async getOrdersByUserId(userId, limit, offset) {
