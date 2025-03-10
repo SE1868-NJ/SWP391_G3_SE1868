5a1c105da43218c29d022bafbc89293263dfc2a4
NhanPhan <ptn987654321@gmail.com>
2025-03-10 20:01:11 +0700

    Update

--- src/models/shop.js
+++ src/models/shop.js
@@ -4,12 +4,10 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
 	class Shop extends Model {
 		static associate(models) {
-			// Shop.hasMany(models.Product, { foreignKey: 'shop_id', onDelete: 'CASCADE' });
 			Shop.belongsTo(models.User, { foreignKey: 'user_id', onDelete: 'CASCADE', });
 			Shop.hasMany(models.Product, { foreignKey: 'shop_id', as: 'products' });
 			Shop.hasMany(models.Banner, { foreignKey: 'shop_id', as: 'banners' });
 			Shop.hasMany(models.Follow, { foreignKey: 'shop_id', as: 'followers' });
-			Shop.hasMany(models.Page, { foreignKey: 'shop_id', as: 'pages' });
 		}
 	}
 	Shop.init(
