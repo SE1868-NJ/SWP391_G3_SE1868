e6b71ad08e630948bc9b437aac9d180f9eb6a01c
NhanPhan <ptn987654321@gmail.com>
2025-03-18 00:13:57 +0700

    Fix }

--- src/controller/orderController.js
+++ src/controller/orderController.js
@@ -2,53 +2,53 @@ const orderService = require("../services/orderService");
 const BaseController = require("./baseController");
 
 class OrderController extends BaseController {
-    createOrder = async (req, res) => {
-        try {
-            const order = req.body;
+  createOrder = async (req, res) => {
+    try {
+      const order = req.body;
 
-            const result = await orderService.createOrder(order);
-            this.convertToJson(res, 200, result);
-        } catch (error) {
-            this.handleError(res, error);
-        }
-    };
-
-    getOrdersByUserId = async (req, res) => {
-        try {
-            const userId = req.params.id;
-
-            const result = await orderService.getOrdersByUserId(userId);
-            this.convertToJson(res, 200, result);
-        } catch (error) {
-            this.handleError(res, error);
-        }
-    };
-
-    getCompletedOrders = async (req, res) => {
-        try {
-            const userId = req.params.id;
-            const result = await orderService.getCompletedOrders(userId);
-
-            if (!result) {
-                return this.convertToJson(res, 404, {
-                    message: "No completed order found for this user",
-                });
-            }
-            this.convertToJson(res, 200, result);
-        } catch (error) {
-            this.handleError(res, error);
-        }
+      const result = await orderService.createOrder(order);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
     }
+  };
 
-    getCancelledOrders = async (req, res) => {
-        try {
-            const userId = req.params.id;
-            const result = await orderService.getCancelledOrders(userId);
-            this.convertToJson(res, 200, result);
-        } catch (error) {
-            this.handleError(res, error);
-        }
+  getOrdersByUserId = async (req, res) => {
+    try {
+      const userId = req.params.id;
+
+      const result = await orderService.getOrdersByUserId(userId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
     }
+  };
+
+  getCompletedOrders = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const result = await orderService.getCompletedOrders(userId);
+
+      if (!result) {
+        return this.convertToJson(res, 404, {
+          message: "No completed order found for this user",
+        });
+      }
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
+
+  getCancelledOrders = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const result = await orderService.getCancelledOrders(userId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
 
   getAllOrders = async (req, res) => {
     try {
@@ -60,24 +60,24 @@ class OrderController extends BaseController {
     }
   };
 
-    cancelOrder = async (req, res) => {
-        try {
-            const orderId = req.params.id;
-            const result = await orderService.cancelOrder(orderId);
-            this.convertToJson(res, 200, { message: 'Order cancelled successfully', data: result });
-        } catch (error) {
-            this.handleError(res, error);
-        }
+  cancelOrder = async (req, res) => {
+    try {
+      const orderId = req.params.id;
+      const result = await orderService.cancelOrder(orderId);
+      this.convertToJson(res, 200, { message: 'Order cancelled successfully', data: result });
+    } catch (error) {
+      this.handleError(res, error);
     }
+  }
 
-    getPendingPaymentOrders = async (req, res) => {
-        try {
-            const userId = req.params.id;
-            const result = await orderService.getPendingPaymentOrders(userId);
-            this.convertToJson(res, 200, result);
-        } catch (error) {
-            this.handleError(res, error);
-        }
-    };
-
+  getPendingPaymentOrders = async (req, res) => {
+    try {
+      const userId = req.params.id;
+      const result = await orderService.getPendingPaymentOrders(userId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
+}
 module.exports = new OrderController();
