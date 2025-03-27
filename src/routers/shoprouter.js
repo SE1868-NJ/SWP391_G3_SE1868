4f7bf7f31fd14d036069dd3bbd9ae6ae8d40fb93
Tran Xuan Dong <dongtx04@gmail.com>
2025-03-28 03:49:40 +0700

    done delivery order

--- src/routers/shoprouter.js
+++ src/routers/shoprouter.js
@@ -39,6 +39,7 @@ router.get('/order/get_new_order_by_shop/:shopId', shopController.getNewOrderByS
 router.get('/order/get_processing_order_by_shop/:shopId', shopController.getProcessingOrderByShop);
 router.get('/order/get_completed_order_by_shop/:shopId', shopController.getCompletedOrdersByShop);
 router.get('/order/get_cancelled_order_by_shop/:shopId', shopController.getCancelledOrdersByShop);
+router.get('/order/get_delivery_order_by_shop/:shopId', shopController.getDeliveryOrdersByShop);
 router.post('/order/update_order_status/:orderId', shopController.updateStatusOrder);
 
 module.exports = router;
