2c86882c6d36453b1d93761dfa8cbbd260b9ae62
NhanPhan <ptn987654321@gmail.com>
2025-03-29 15:30:25 +0700

    Merge main

--- src/controller/ShopController.js
+++ src/controller/ShopController.js
@@ -9,156 +9,156 @@ const path = require('path');
 const fs = require('fs');
 
 class ShopController extends BaseController {
-	getCategory = async (req, res) => {
-		try {
-			const categories = await categoryService.getAllCategory();
-			return this.convertToJson(res, 200, categories);
-		} catch (error) {
-			return this.handleError(res, error);
-		}
-	};
+  getCategory = async (req, res) => {
+    try {
+      const categories = await categoryService.getAllCategory();
+      return this.convertToJson(res, 200, categories);
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 
-	getAllProduct = async (req, res) => {
-		try {
-			const products = await productService.getAllProduct();
-			return this.convertToJson(res, 200, products);
-		} catch (error) {
-			return this.handleError(res, error);
-		}
-	};
-	getProducts = async (req, res) => {
-		try {
-			const params = {
-				page: parseInt(req.body.page),
-				limit: parseInt(req.body.limit) || 4, // Giữ nguyên mặc định nếu không có limit
-				search: req.body.search || "", // Lấy thêm search từ body
-				sortPrice: req.body.sortPrice || "asc", // Lấy thêm sortPrice từ body\ categories: req.body.categories || ""
-				minPrice: req.body.minPrice || 0, // Giá thấp nhất
-				maxPrice: req.body.maxPrice || 10000000, // Giá cao nhất (default rất lớn)
-			};
+  getAllProduct = async (req, res) => {
+    try {
+      const products = await productService.getAllProduct();
+      return this.convertToJson(res, 200, products);
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
+  getProducts = async (req, res) => {
+    try {
+      const params = {
+        page: parseInt(req.body.page),
+        limit: parseInt(req.body.limit) || 4, // Giữ nguyên mặc định nếu không có limit
+        search: req.body.search || "", // Lấy thêm search từ body
+        sortPrice: req.body.sortPrice || "asc", // Lấy thêm sortPrice từ body\ categories: req.body.categories || ""
+        minPrice: req.body.minPrice || 0, // Giá thấp nhất
+        maxPrice: req.body.maxPrice || 10000000, // Giá cao nhất (default rất lớn)
+      };
 
-			const result = await productService.getProducts(params);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	};
+      const result = await productService.getProducts(params);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
 
-	getProductById = async (req, res) => {
-		try {
-			const id = parseInt(req.params.id);
+  getProductById = async (req, res) => {
+    try {
+      const id = parseInt(req.params.id);
 
-			// Tăng search_count chỉ khi product tồn tại
-			const product = await productService.getProductById(id);
+      // Tăng search_count chỉ khi product tồn tại
+      const product = await productService.getProductById(id);
 
-			if (!product) {
-				return this.convertToJson(res, 404, { message: "Product not found" });
-			}
+      if (!product) {
+        return this.convertToJson(res, 404, { message: "Product not found" });
+      }
 
-			await productService.increaseSearchCount(id);
+      await productService.increaseSearchCount(id);
 
-			this.convertToJson(res, 200, product);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	};
+      this.convertToJson(res, 200, product);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
 
-	getProductByName = async (req, res) => {
-		try {
-			const productName = req.params.name;
+  getProductByName = async (req, res) => {
+    try {
+      const productName = req.params.name;
 
-			const result = await productService.getProductByName(productName);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	};
+      const result = await productService.getProductByName(productName);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
 
-	createCart = async (req, res) => {
-		try {
-			const cart = req.body;
+  createCart = async (req, res) => {
+    try {
+      const cart = req.body;
 
-			const result = await productService.createCart(cart);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	}
+      const result = await productService.createCart(cart);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
 
-	getCartsByUserId = async (req, res) => {
-		try {
-			const userId = req.params.id;
+  getCartsByUserId = async (req, res) => {
+    try {
+      const userId = req.params.id;
 
-			const result = await cartService.getCartsByUserId(userId);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	}
+      const result = await cartService.getCartsByUserId(userId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
 
-	updateCartQuantity = async (req, res) => {
-		try {
-			const cartId = req.params.cartId;
-			const { quantity } = req.body;
-			const result = await cartService.updateCartQuantity(cartId, quantity);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	}
+  updateCartQuantity = async (req, res) => {
+    try {
+      const cartId = req.params.cartId;
+      const { quantity } = req.body;
+      const result = await cartService.updateCartQuantity(cartId, quantity);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
 
-	removeCartItem = async (req, res) => {
-		try {
-			const cartId = req.params.cartId;
-			const result = await cartService.removeCartItem(cartId);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	}
+  removeCartItem = async (req, res) => {
+    try {
+      const cartId = req.params.cartId;
+      const result = await cartService.removeCartItem(cartId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
 
-	removeMultipleCartItems = async (req, res) => {
-		try {
-			const { cartIds } = req.body;
-			const result = await cartService.removeMultipleCartItems(cartIds);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	}
+  removeMultipleCartItems = async (req, res) => {
+    try {
+      const { cartIds } = req.body;
+      const result = await cartService.removeMultipleCartItems(cartIds);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
 
-	getCountCartByUserId = async (req, res) => {
-		try {
-			const userId = req.params.id;
+  getCountCartByUserId = async (req, res) => {
+    try {
+      const userId = req.params.id;
 
-			const result = await productService.getCountCartByUserId(userId);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	};
+      const result = await productService.getCountCartByUserId(userId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
 
-	getShopByUserId = async (req, res) => {
-		try {
-			const userId = req.params.id;
+  getShopByUserId = async (req, res) => {
+    try {
+      const userId = req.params.id;
 
-			const result = await shopService.getShopsByUserId(userId);
-			this.convertToJson(res, 200, result);
-		} catch (error) {
-			this.handleError(res, error);
-		}
-	};
+      const result = await shopService.getShopsByUserId(userId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  };
 
-	updateShop = async (req, res) => {
-		try {
-			const shopId = parseInt(req.params.id);
-			let shopData = {
-				shop_name: req.body.shop_name,
-				shop_description: req.body.shop_description,
-				shop_address: req.body.shop_address,
-				shop_email: req.body.shop_email,
-				shop_phone: req.body.shop_phone
-			};
+  updateShop = async (req, res) => {
+    try {
+      const shopId = parseInt(req.params.id);
+      let shopData = {
+        shop_name: req.body.shop_name,
+        shop_description: req.body.shop_description,
+        shop_address: req.body.shop_address,
+        shop_email: req.body.shop_email,
+        shop_phone: req.body.shop_phone
+      };
 
       if (req.files && req.files.shop_logo) {
         const file = req.files.shop_logo;
@@ -189,43 +189,43 @@ class ShopController extends BaseController {
         shopData.shop_logo = `/uploads/shop_logos/${fileName}`;
       }
 
-			const result = await shopService.updateShop(shopId, shopData);
-			return this.convertToJson(res, 200, result);
-		} catch (error) {
-			return this.handleError(res, error);
-		}
-	};
+      const result = await shopService.updateShop(shopId, shopData);
+      return this.convertToJson(res, 200, result);
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 
-	getTopSearchedProducts = async (req, res) => {
-		try {
-			const limit = parseInt(req.query.limit) || 4; // Mặc định là 4 sản phẩm
+  getTopSearchedProducts = async (req, res) => {
+    try {
+      const limit = parseInt(req.query.limit) || 4; // Mặc định là 4 sản phẩm
 
-			const result = await productService.getMostSearchedProducts(limit);
-			return this.convertToJson(res, 200, result);
-		} catch (error) {
-			return this.handleError(res, error);
-		}
-	};
+      const result = await productService.getMostSearchedProducts(limit);
+      return this.convertToJson(res, 200, result);
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 
-	increaseSearchCount = async (req, res) => {
-		try {
-			const productId = parseInt(req.params.id);
-			await productService.increaseSearchCount(productId);
-			return this.convertToJson(res, 200, { message: "Search count updated" });
-		} catch (error) {
-			return this.handleError(res, error);
-		}
-	};
+  increaseSearchCount = async (req, res) => {
+    try {
+      const productId = parseInt(req.params.id);
+      await productService.increaseSearchCount(productId);
+      return this.convertToJson(res, 200, { message: "Search count updated" });
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 
-	getShopHomepage = async (req, res) => {
-		try {
-			const shopId = parseInt(req.params.id);
-			const result = await shopService.getShopHomepage(shopId);
-			return this.convertToJson(res, 200, result);
-		} catch (error) {
-			return this.handleError(res, error);
-		}
-	};
+  getShopHomepage = async (req, res) => {
+    try {
+      const shopId = parseInt(req.params.id);
+      const result = await shopService.getShopHomepage(shopId);
+      return this.convertToJson(res, 200, result);
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 
   getProductsByShopAndCategory = async (req, res) => {
     try {
@@ -243,15 +243,84 @@ class ShopController extends BaseController {
 
   getFeedbacksByShop = async (req, res) => {
     try {
-        const shopId = req.params.id;
-        const { startDate, endDate } = req.query;
+      const shopId = req.params.id;
+      const { startDate, endDate } = req.query;
 
-        const feedbacks = await shopService.getFeedbacksByShop(shopId, startDate, endDate);
-        return this.convertToJson(res, 200, feedbacks);
+      const feedbacks = await shopService.getFeedbacksByShop(shopId, startDate, endDate);
+      return this.convertToJson(res, 200, feedbacks);
     } catch (error) {
-        return this.handleError(res, error);
+      return this.handleError(res, error);
     }
-};
+  };
+
+
+  getNewOrderByShop = async (req, res) => {
+    try {
+      const shopId = req.params.shopId;
+
+      const result = await orderService.getNewOrderByShop(shopId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
+
+  getProcessingOrderByShop = async (req, res) => {
+    try {
+      const shopId = req.params.shopId;
+
+      const result = await orderService.getProcessingOrderByShop(shopId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
+
+  getCompletedOrdersByShop = async (req, res) => {
+    try {
+      const shopId = req.params.shopId;
+
+      const result = await orderService.getCompletedOrdersByShop(shopId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
+
+  getCancelledOrdersByShop = async (req, res) => {
+    try {
+      const shopId = req.params.shopId;
+
+      const result = await orderService.getCancelledOrdersByShop(shopId);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
+
+  updateStatusOrder = async (req, res) => {
+    try {
+      const orderId = req.params.orderId;
+      const { status } = req.body;
+
+      const result = await orderService.updateOrderStatus(orderId, status);
+      this.convertToJson(res, 200, result);
+    } catch (error) {
+      this.handleError(res, error);
+    }
+  }
+
+  getFeedbacksByShop = async (req, res) => {
+    try {
+      const shopId = req.params.id;
+      const { startDate, endDate } = req.query;
+
+      const feedbacks = await shopService.getFeedbacksByShop(shopId, startDate, endDate);
+      return this.convertToJson(res, 200, feedbacks);
+    } catch (error) {
+      return this.handleError(res, error);
+    }
+  };
 
   getDeliveryOrdersByShop = async (req, res) => {
     try {
@@ -262,7 +331,7 @@ class ShopController extends BaseController {
     } catch (error) {
       this.handleError(res, error);
     }
-  }
+  };
 
 }
 module.exports = new ShopController();
